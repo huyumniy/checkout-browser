521ad0b28752b719a3defb40992bc8e88d7536d4
huyumniy <vladkg53@gmail.com>
2024-07-09 10:15:32 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import undetected_chromedriver as webdriver
-import os, sys
+import os
+import sys
 import ast
 from urllib.parse import urlparse
 import eel
 import tempfile
 import shutil
 from selenium.webdriver.common.by import By
-import undetected_chromedriver as webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -88,51 +87,58 @@ class ProxyExtension:
 def selenium_connect(link, proxy='', user_agent='', cookie_string=''):
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
-    #options.add_argument("--incognito")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--log-level=3")
     options.add_argument("--disable-web-security")
     options.add_argument("--disable-site-isolation-trials")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--lang=EN')
-    print(user_agent)
-    if user_agent: options.add_argument(f'--user-agent={user_agent}')
-    cwd= os.getcwd()
+
+    if user_agent:
+        options.add_argument(f'--user-agent={user_agent}')
+    
+    cwd = os.getcwd()
     slash = "\\" if sys.platform == "win32" else "/"
     directory_name = cwd + slash + "uBlock-Origin"
     extension = os.path.join(cwd, directory_name)
-    print(extension)
+
     if proxy:
         proxy = proxy.split(":", 3)
         proxy[1] = int(proxy[1])
-        print(proxy)
         proxy_extension = ProxyExtension(*proxy)
         options.add_argument(f"--load-extension={proxy_extension.directory},{extension}")
-    else: options.add_argument(f"--load-extension={extension}")
+    else:
+        options.add_argument(f"--load-extension={extension}")
 
-    prefs = {"credentials_enable_service": False,
-        "profile.password_manager_enabled": False}
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False
+    }
     options.add_experimental_option("prefs", prefs)
+
     try:
-        driver = webdriver.Chrome(
-            options=options,
-            enable_cdp_events=True
-        )
-    except: driver.quit()
-    
-    # Convert cookie string to dictionary
+        driver = webdriver.Chrome(options=options, enable_cdp_events=True)
+    except Exception as e:
+        print(f"Error initializing driver: {e}")
+        return None
+
     if cookie_string:
-        parsed_url = urlparse(link)
-        cookie_string = ast.literal_eval(cookie_string)
-        main_domain = parsed_url.scheme + "://" + cookie_string[0]['domain'].split('.', 1)[1]
-        print(main_domain)
-        driver.get(main_domain)
-        
-        for cookie in cookie_string:
-            print(cookie)
-            driver.add_cookie(cookie)
+        try:
+            parsed_url = urlparse(link)
+            cookie_list = ast.literal_eval(cookie_string)
+            main_domain = parsed_url.scheme + "://" + parsed_url.netloc
+
+            driver.get(main_domain)
+
+            for cookie in cookie_list:
+                driver.add_cookie(cookie)
+        except Exception as e:
+            print(f"Error parsing cookies: {e}")
+            print(f"cookie_string: {type(cookie_string)}")
+            return driver
 
     return driver
+
 
 def read_file():
     file_path = os.path.join(os.getcwd(), 'info.txt')
@@ -152,25 +158,31 @@ def check_for_element(driver, selector, click=False, xpath=False):
             element = driver.find_element(By.XPATH, selector)
         else:
             element = driver.find_element(By.CSS_SELECTOR, selector)
-        if click: 
+        if click:
             driver.execute_script("arguments[0].scrollIntoView();", element)
             element.click()
         return element
-    except: return False
+    except Exception as e:
+        print(f"Error finding element: {e}")
+        return False
 
 
 @eel.expose
 def main(user_agent, cookies, link, proxy):
     driver = selenium_connect(link, proxy, user_agent, cookies)
+    if driver is None:
+        print("Failed to initialize the driver.")
+        return
+
     driver.get(link)
-    try:
-        check_for_element(driver, "//button[contains(@class, 'sc-aitciz-1') and contains(@class, 'hWHZkc')][2]", xpath=True, click=True)
-    except: print("did not manage to click on 'Find seats for me'")
+    # try:
+    #     check_for_element(driver, "//button[contains(@class, 'sc-aitciz-1') and contains(@class, 'hWHZkc')][2]", xpath=True, click=True)
+    # except Exception as e:
+    #     print(f"did not manage to click on 'Find seats for me': {e}")
+    
     input('Enter to quit()')
     driver.quit()
 
 if __name__ == '__main__':
-    # data = read_file()
     eel.init('front')
-    eel.start('main.html', size=(600, 800))
-   
+    eel.start('main.html', size=(600, 800), port=8080)
